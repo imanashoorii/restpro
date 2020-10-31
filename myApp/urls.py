1ac6628cef1f650f9386bd11b070eebb7a307e3d
Iman Ashoori <56173335+imanashoorii@users.noreply.github.com>
2020-10-31 08:20:30 -0700

    init

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from myApp import views
 
 urlpatterns = [
-    path("hello-world", views.hello_world)
+    path("hello-world", views.hello_world),
+    # 127.0.0.1:8000/hello-world
+    path("hello", views.hello),
+    # 127.0.0.1:8000/hello
 ]
