aec3f2e51b72128f5c6da02fd483beee81e43e57
imanashoorii <imanashoorii.77@gmail.com>
2020-11-24 22:34:07 +0330

    adding serializers.py

--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import CalcSerializer
 
 
 @api_view()
@@ -16,28 +17,50 @@ def hello(request):
         return Response({'message': 'hello {}'.format(request.data['name'])})
 
 
-@api_view(['GET', 'POST'])
+# ---------- Without Serializer ----------
+
+# @api_view(['GET', 'POST'])
+# def calculator(request):
+#     try:
+#         num1 = request.data['num1']
+#         num2 = request.data['num2']
+#         opr = request.data['opr']
+#     except:
+#         return Response({"error": "enter commands correctly"},
+#                         status=status.HTTP_400_BAD_REQUEST)
+#
+#     else:
+#         if isinstance(num1, int) and isinstance(num2, int):
+#
+#             if opr == '+':
+#                 return Response({"result": num1 + num2}, status=status.HTTP_200_OK)
+#             elif opr == '-':
+#                 return Response({"result": num1 - num2}, status=status.HTTP_200_OK)
+#             elif opr == '*':
+#                 return Response({"result": num1 * num2}, status=status.HTTP_200_OK)
+#             else:
+#                 return Response({"error": "wrong operator"},
+#                                 status=status.HTTP_400_BAD_REQUEST)
+#         else:
+#             return Response({"error": "Send integer values"},
+#                             status=status.HTTP_400_BAD_REQUEST)
+
+
+# ---------- With Serializer ----------
+@api_view(['POST'])
 def calculator(request):
-    try:
-        num1 = request.data['num1']
-        num2 = request.data['num2']
-        opr = request.data['opr']
-    except:
-        return Response({"error": "enter commands correctly"},
-                        status=status.HTTP_400_BAD_REQUEST)
+    ser = CalcSerializer(data=request.data)
 
+    if ser.is_valid():
+        num1 = ser.data['num1']
+        num2 = ser.data['num1']
+        opr = ser.data['opr']
+
+        if opr == '+':
+            return Response({"result": num1 + num2}, status=status.HTTP_200_OK)
+        elif opr == '-':
+            return Response({"result": num1 - num2}, status=status.HTTP_200_OK)
+        elif opr == '*':
+            return Response({"result": num1 * num2}, status=status.HTTP_200_OK)
     else:
-        if isinstance(num1, int) and isinstance(num2, int):
-
-            if opr == '+':
-                return Response({"result": num1 + num2}, status=status.HTTP_200_OK)
-            elif opr == '-':
-                return Response({"result": num1 - num2}, status=status.HTTP_200_OK)
-            elif opr == '*':
-                return Response({"result": num1 * num2}, status=status.HTTP_200_OK)
-            else:
-                return Response({"error": "wrong operator"},
-                                status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({"error": "Send integer values"},
-                            status=status.HTTP_400_BAD_REQUEST)
+        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
